f4d2aaa36c3da9c4ddf4a2c4eb58d352f748d390
apvenkat <venkat.subramanian94@gmail.com>
2019-11-14 20:38:29 +0800

    remove old configs

--- routes/api.js
+++ routes/api.js
@@ -1,46 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('db/sqlitedb.db');
-var bodyParser = require('body-parser');
-const Gpio = require('onoff').Gpio;
-var fs = require('fs');
-var gpiodata = require('../config/gpio-config.json');
+var sqlite3 = require("sqlite3");
+var db = new sqlite3.Database("db/sqlitedb.db");
+var bodyParser = require("body-parser");
+const Gpio = require("onoff").Gpio;
+// var fs = require('fs');
+// var gpiodata = require('../config/gpio-config.json');
 
-router.get('/api', function(req, res) {
+router.get("/api", function(req, res) {
   res.json(gpiodata);
 });
 
-
-
-
 //Add Device
-router.post('/api',function(req ,res){
+router.post("/api", function(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
-var pin = req.body.pin;
-var name = req.body.name;
-var type = req.body.type;
-var description = req.body.description;
-var value = req.body.value;
+  var pin = req.body.pin;
+  var name = req.body.name;
+  var type = req.body.type;
+  var description = req.body.description;
+  var value = req.body.value;
 
-var sql = `insert into gpiolist (name, description,pin, type, value)
+  var sql = `insert into gpiolist (name, description,pin, type, value)
       VALUES
       (?,?,?, ?, ?);`;
 
-var values = [name,description,pin,type, value];
+  var values = [name, description, pin, type, value];
 
-db.serialize(function () {
-  db.run(sql, values, function (err) {
-      if (err){
-          console.error(err);
-          res.status(500).send(err);
-      }
-
-      else
-          res.send();
+  db.serialize(function() {
+    db.run(sql, values, function(err) {
+      if (err) {
+        console.error(err);
+        res.status(500).send(err);
+      } else res.send();
+    });
   });
-});
 });
 
 router.use(bodyParser.json());
@@ -56,78 +50,66 @@ router.use(bodyParser.urlencoded({ extended: false }));
 //   res.json(gpiodata);
 // });
 //Delete Device
-router.delete('/api/delete/:id',function(req ,res){
-    res.setHeader("Access-Control-Allow-Origin", "*");
-  var id=req.params.id;
-  if(!id){
-          res.status(400).send("ID is mandatory");
-      }
-      else{
-      var sql = `delete from  gpiolist where id = ?;`;
-      var values = [id];
-  
-      db.serialize(function () {
-          db.run(sql, values, function (err) {
-              if (err){
-                  console.error(err);
-                  res.status(500).send(err);
-              }
-              else
-                  res.send();
-          });
+router.delete("/api/delete/:id", function(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  var id = req.params.id;
+  if (!id) {
+    res.status(400).send("ID is mandatory");
+  } else {
+    var sql = `delete from  gpiolist where id = ?;`;
+    var values = [id];
+
+    db.serialize(function() {
+      db.run(sql, values, function(err) {
+        if (err) {
+          console.error(err);
+          res.status(500).send(err);
+        } else res.send();
       });
+    });
   }
-  });
-  
-  //Update device
-  router.put('/api/id/:id',function(req ,res){
-    res.setHeader("Access-Control-Allow-Origin", "*");
-  
-    var pin = req.body.pin;
-    var description = req.body.description;
-        var name = req.body.name;
-        var value = req.body.value;
-        var id = req.params.id;
-  
-        var sql = `update gpiolist
+});
+
+//Update device
+router.put("/api/id/:id", function(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  var pin = req.body.pin;
+  var description = req.body.description;
+  var name = req.body.name;
+  var value = req.body.value;
+  var id = req.params.id;
+
+  var sql = `update gpiolist
                 set pin = ?, description = ?, name = ?, value = ?
                 where id = ?;`;
-  
-        var values = [pin,description, name, value, id];
-  
-        db.serialize(function () {
-            db.run(sql, values, function (err) {
-                if (err){
-                    console.error(err);
-                    res.status(500).send(err);
-                }
-                else
-                    res.send();
-            });
-        });
+
+  var values = [pin, description, name, value, id];
+
+  db.serialize(function() {
+    db.run(sql, values, function(err) {
+      if (err) {
+        console.error(err);
+        res.status(500).send(err);
+      } else res.send();
+    });
   });
-  
+});
 
 //GPIO high
 
-router.post('/api/on/:id', function(req, res) {
-  const led = new Gpio(gpiodata[req.params.id].gpio, 'out');
+router.post("/api/on/:id", function(req, res) {
+  const led = new Gpio(gpiodata[req.params.id].gpio, "out");
   led.writeSync(1);
-res.sendStatus(200);
-
+  res.sendStatus(200);
 });
 
 //GPIO low
 
-router.post('/api/off/:id', function(req, res) {
-  const led = new Gpio(gpiodata[req.params.id].gpio, 'out');
+router.post("/api/off/:id", function(req, res) {
+  const led = new Gpio(gpiodata[req.params.id].gpio, "out");
   led.writeSync(0);
-res.sendStatus(200);
-  
+  res.sendStatus(200);
 });
-
-
-
-
 
 module.exports = router;
