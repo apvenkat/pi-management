c4badf46420d9340b3acc803e53eb1efe72e1b30
apvenkat <venkat.subramanian94@gmail.com>
2019-12-13 22:42:24 +0800

    added jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,14 +2,10 @@ const jwt = require("jsonwebtoken");
 var secret = "This is the secret for signing tokens";
 
 module.exports = function(req, res, next) {
-  const authorizationHeaader = req.headers.authorization;
-  //
+  const token = req.cookies.token;
 
-  if (authorizationHeaader) {
-    const token = req.headers.authorization.split(" ")[1];
-    if (!token) {
-      return res.status(401).send("Access denied. No JWT provided.");
-    }
+  if (!token) {
+    return res.status(401).send("Access denied. No JWT provided.");
   }
 
   try {
