08cdce7a39c1986fab12bbfb98b2c34a561bf289
apvenkat <venkat.subramanian94@gmail.com>
2019-12-14 13:18:00 +0800

    added buttons

--- public/config.js
+++ public/config.js
@@ -60,6 +60,7 @@ $(function() {
       output +=
         '       <div class="media-left"><button class="feedback-delete btn btn-xs btn-danger"><span id="' +
         key +
+        1 +
         '" class="glyphicon glyphicon-remove"></span></button></div>';
       output += '         <div class="feedback-info media-body">';
       output += '           <div class="feedback-head">';
