ec5539501ea3de0b0de1b25a8b06c169af88f672
apvenkat <venkat.subramanian94@gmail.com>
2019-12-16 22:45:40 +0800

    added client auth

--- routes/users.js
+++ routes/users.js
@@ -86,7 +86,7 @@ router.post("/LoginUser", function(req, res) {
           } else {
             var token = jwt.sign({ email: req.body.email }, secret);
             res.cookie("token", token);
-            res.header("x-auth-token", token);
+            res.header("Authorization", "Bearer" + token);
             res.redirect("/dashboard");
           }
         });
