de2b880a1679afc0a46e370cf07cf77ca71251a0
apvenkat <venkat.subramanian94@gmail.com>
2019-12-16 21:09:40 +0800

    redirect

--- public/login.js
+++ public/login.js
@@ -1,1 +1,28 @@
-//
+$(function() {
+  $(".login").submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: "LoginUser",
+      dataType: "json",
+      type: "POST",
+      success: function() {
+        $.ajax({
+          url: "dashboard",
+          header: {
+            "X-Auth-Token": $.cookie("token")
+          },
+          dataType: "json",
+          type: "GET",
+          success: (window.location = "dashboard")
+        });
+      }
+    }); //feedback messages
+
+    // function alert() {
+    //     var output = "";
+    //     output +=
+    //         '<div class="alert alert-success" role="alert"> Data Inserted Successfully !!</div>';
+    //     $(".alert-messages").html(output);
+    // }
+  });
+});
